a7bbce203eb5368f13718f19d09cdb8137e17f84
tforce7171 <68318576+tforce7171@users.noreply.github.com>
2021-07-14 23:33:42 +0900

    Add files via upload

--- windows_controller.py
+++ windows_controller.py
@@ -10,13 +10,11 @@ def WindowsController(base_url, g4dn_instance_id, t2micro_instance_id):
         replay_data = GetReplayData(base_url)
         if ReplayInQueue(replay_data):
             StartInstance(g4dn_instance_id)
-            OpenRDPConn(t2micro_instance_id)
             while True:
                 time.sleep(5)
                 replay_data = GetReplayData(base_url)
-                if not ReplayInQueue(replay_data):
+                if not ReplayInQueue(replay_data) and not ReplayInProcess(replay_data):
                     StopInstance(g4dn_instance_id)
-                    CloseRDPConn(t2micro_instance_id)
                     break
 
 def GetReplayData(base_url):
@@ -31,6 +29,14 @@ def ReplayInQueue(replay_data):
             replay_in_queue = True
             print(f'replay in queue {replay_in_queue}')
     return replay_in_queue
+
+def ReplayInProcess(replay_data):
+    replay_in_process = False
+    for data in replay_data:
+        if data["conversion_status"] == "in process":
+            replay_in_process = True
+            print(f'replay in queue {replay_in_process}')
+    return replay_in_process
 
 def StartInstance(instance_id):
     print(f'starting instance {instance_id}')
@@ -54,27 +60,10 @@ def StopInstance(instance_id):
         if state == "stopped":
             break
 
-def OpenRDPConn(instance_id):
-    public_dns_name = PublicDNSName(instance_id)
-    print(f'sending connection command to {public_dns_name}')
-    result = requests.get("http://" + public_dns_name + "/connect")
-    print(result.json())
-
-def CloseRDPConn(instance_id):
-    public_dns_name = PublicDNSName(instance_id)
-    print(f'sending disconnection command to {public_dns_name}')
-    result = requests.get("http://" + public_dns_name + "/disconnect")
-    print(result.json())
-
 def InstanceState(instance_id):
     instance_status = GetInstanceStatus(instance_id)
     state = instance_status["Reservations"][0]["Instances"][0]["State"]["Name"]
     return state
-
-def PublicDNSName(instance_id):
-    instance_status = GetInstanceStatus(instance_id)
-    public_dns_name = instance_status["Reservations"][0]["Instances"][0]["NetworkInterfaces"][0]["Association"]["PublicDnsName"]
-    return public_dns_name
 
 def GetInstanceStatus(instance_id):
     client = boto3.client('ec2')
@@ -82,7 +71,6 @@ def GetInstanceStatus(instance_id):
     return instance_status
 
 if __name__ == '__main__':
-    base_url = os.environ['BASE_URL']
+    database_url = os.environ['BASE_URL']
     g4dn_instance_id = os.environ['G4DN_INSTANCE_ID']
-    t2micro_instance_id = os.environ['T2MICRO_INSTANCE_ID']
-    WindowsController(base_url, g4dn_instance_id, t2micro_instance_id)
+    WindowsController(base_url, g4dn_instance_id)
