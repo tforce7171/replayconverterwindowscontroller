d128a29ef5a5ea84afa80324cd2c3a1e854754be
tforce7171 <taisei_080@yahoo.co.jp>
2021-08-17 11:45:07 +0900

    bug fix: wrong order on if, elif

--- windows_controller.py
+++ windows_controller.py
@@ -16,11 +16,12 @@ def WindowsController(database_url, g4dn_instance_id):
                 replay_data_in_queue = GetReplayData(database_url, "in queue")
                 replay_data_in_process = GetReplayData(database_url, "in process")
                 replay_data_error = GetReplayData(database_url, "error")
-                if replay_data_in_queue["meta"]["count"] == 0 and replay_data_in_process["meta"]["count"] == 0:
+                if replay_data_error["meta"]["count"] != 0:
+                    sys.exit()
+                elif replay_data_in_queue["meta"]["count"] == 0 and replay_data_in_process["meta"]["count"] == 0:
                     StopInstance(g4dn_instance_id)
                     break
-                elif replay_data_error["meta"]["count"] != 0:
-                    sys.exit()
+
 
 def GetReplayData(database_url, conversion_status):
     body = {"conversion_status": conversion_status}
